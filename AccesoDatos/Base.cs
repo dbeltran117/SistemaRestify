20245bd6608ead06165fb4864a960d952e2b0f6d
Diego Chico <diegochicobeltran@gmail.com>
2025-10-29 09:27:10 -0600

    Creacion de las capas de programacion

---
+++ AccesoDatos/Base.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace AccesoDatos
+{
+    public class Base
+    {
+        MySqlConnection con;
+        public Base(string servidor, string usuario, string password, string baseDatos, int puerto = 3306)
+        {
+            con = new MySqlConnection($"server={servidor}; user={usuario}; password={password}; database={baseDatos}; port={puerto};");
+        }
+
+        public void Comando(string cadena)
+        {
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(cadena, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+        }
+
+        public DataSet Consultar(string consulta, string tabla)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                MySqlDataAdapter da = new MySqlDataAdapter(consulta, con);
+                con.Open();
+                da.Fill(ds, tabla);
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+            return ds;
+        }
+    }
+}
