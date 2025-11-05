06e7fb92289cf9c4250801cdb927e3d6c89a2178
rosad <rosad@COLLAZO>
2025-11-04 21:33:52 -0600

    Diseno Pendiente Terminado

---
+++ SistemaRestify/FrmComedor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaRestify
+{
+    public partial class FrmComedor : Form
+    {
+        public FrmComedor()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtAreaAc_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
